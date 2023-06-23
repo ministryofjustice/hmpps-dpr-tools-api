2920deec7b17cd54b51937c49d2acf38a5779939
Stu Pegg <110236995+stalwartsolutions@users.noreply.github.com>
2023-06-23 11:10:34 +0100

    [DPR-000] OWASP dependency fix (#18)
    * [DPR-000] OWASP dependency fix
    
    * [DPR-000] Linting

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,9 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.1.4"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.2.0"
+  kotlin("jvm") version "1.8.21"
   kotlin("plugin.spring") version "1.8.21"
-  id("org.springframework.boot") version "3.1.0"
-  kotlin("plugin.jpa") version "1.7.21"
+  kotlin("plugin.jpa") version "1.8.21"
 }
-
-apply(plugin = "org.springframework.boot")
-apply(plugin = "org.jetbrains.kotlin.plugin.jpa")
 
 configurations {
   testImplementation { exclude(group = "org.junit.vintage") }
@@ -23,15 +20,13 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
 
   // Swagger
-  implementation("org.springdoc:springdoc-openapi-starter-common:2.1.0")
   implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.1.0")
 
   // Overrides to fix security vulnerabilities
-  implementation("com.google.guava:guava:32.0.1-jre")
-  implementation("com.fasterxml.jackson.core:jackson-databind:2.15.2")
+  implementation("io.netty:netty-handler:4.1.94.Final")
 
   // Testing
-  testImplementation("com.h2database:h2:2.1.214")
+  testImplementation("com.h2database:h2")
   testImplementation("io.jsonwebtoken:jjwt:0.9.1")
   testImplementation("javax.xml.bind:jaxb-api:2.4.0-b180830.0359")
 }
