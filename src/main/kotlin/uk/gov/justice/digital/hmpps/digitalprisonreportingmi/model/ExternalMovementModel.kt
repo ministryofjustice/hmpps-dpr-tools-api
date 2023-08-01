bdc987db9a7688bd10a2cfe2cdc16642159d5e0d
Gavriil <135604838+gavriil-g-moj@users.noreply.github.com>
2023-08-01 10:08:25 +0100

    Dpr 542 - Accept nulls for origin destination and direction (#31)
    * DPR-542 created the real ExternalMovementRepository which will replace the FakeExternalMovementRepository - WIP
    
    * DPR-542 changed prisonNumber to prisoner as per the data in Redshift and added direction condition to the query- WIP
    
    * DPR-542 preparedStatement in the where clause, added the remaining tests to ExternalMovementRepositoryTest, split model and entity, disabled FakeExternalMovementRepositoryTests
    
    * DPR-542 deleted FakeExternalMovementRepository, FakeExternalMovementRepositoryTest and the fake data json files
    
    * DPR-542 move sortColumn mapping and validation to service from repository and added tests
    
    * DPR-542 adjust query to sort by time only and not both date and time when time is chosen as the sorting column
    
    * DPR-542 handle nulls for origin destination and direction

--- src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportingmi/model/ExternalMovementModel.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportingmi/model/ExternalMovementModel.kt
@@ -9,9 +9,9 @@ data class ExternalMovementModel(
   val prisonNumber: String,
   val date: LocalDate,
   val time: LocalTime,
-  val from: String,
-  val to: String,
-  val direction: String,
+  val from: String?,
+  val to: String?,
+  val direction: String?,
   val type: String,
   val reason: String,
 )
