0c09cad1f5990706095bdd3de29215a35d91b43a
Stu Pegg <stuart.pegg@digital.justice.gov.uk>
2023-12-04 11:47:56 +0000

    [DPR2-198] Library version bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
   implementation("com.amazon.redshift:redshift-jdbc4-no-awssdk:1.2.45.1069")
-  implementation("uk.gov.justice.service.hmpps:hmpps-digital-prison-reporting-lib:1.1.0")
+  implementation("uk.gov.justice.service.hmpps:hmpps-digital-prison-reporting-lib:1.1.1")
 
   // Security
   implementation("org.springframework.boot:spring-boot-starter-security")
