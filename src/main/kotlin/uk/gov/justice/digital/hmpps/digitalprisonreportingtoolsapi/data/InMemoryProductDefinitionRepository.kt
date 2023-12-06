fd0bec7b8c22be037b119337cd23762e41580a61
Gavriil <135604838+gavriil-g-moj@users.noreply.github.com>
2023-12-06 10:16:33 +0000

    Use ConcurrentHashMap instead of MutableMap for thread safe map retrieval and modification and update lib to reuse code (#2)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportingtoolsapi/data/InMemoryProductDefinitionRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportingtoolsapi/data/InMemoryProductDefinitionRepository.kt
@@ -2,18 +2,14 @@ package uk.gov.justice.digital.hmpps.digitalprisonreportingtoolsapi.data
 
 import jakarta.validation.ValidationException
 import org.springframework.stereotype.Service
-import uk.gov.justice.digital.hmpps.digitalprisonreportinglib.data.ProductDefinitionRepository
+import uk.gov.justice.digital.hmpps.digitalprisonreportinglib.data.AbstractProductDefinitionRepository
 import uk.gov.justice.digital.hmpps.digitalprisonreportinglib.data.model.ProductDefinition
-import uk.gov.justice.digital.hmpps.digitalprisonreportinglib.data.model.SingleReportProductDefinition
+import java.util.concurrent.ConcurrentHashMap
 
 @Service
-class InMemoryProductDefinitionRepository : ProductDefinitionRepository {
+class InMemoryProductDefinitionRepository : AbstractProductDefinitionRepository() {
 
-  companion object {
-    private const val schemaRefPrefix = "\$ref:"
-  }
-
-  private val definitions: MutableMap<String, ProductDefinition> = HashMap()
+  private val definitions: ConcurrentHashMap<String, ProductDefinition> = ConcurrentHashMap()
 
   override fun getProductDefinitions(): List<ProductDefinition> {
     return definitions.values.toList()
@@ -21,30 +17,6 @@ class InMemoryProductDefinitionRepository : ProductDefinitionRepository {
 
   override fun getProductDefinition(definitionId: String): ProductDefinition =
     definitions.getOrElse(definitionId) { throw ValidationException("Invalid report id provided: $definitionId") }
-
-  override fun getSingleReportProductDefinition(definitionId: String, reportId: String): SingleReportProductDefinition {
-    val productDefinition = getProductDefinition(definitionId)
-    val reportDefinition = productDefinition.report
-      .filter { it.id == reportId }
-      .ifEmpty { throw ValidationException("Invalid report variant id provided: $reportId") }
-      .first()
-
-    val dataSetId = reportDefinition.dataset.removePrefix(schemaRefPrefix)
-    val dataSet = productDefinition.dataset
-      .filter { it.id == dataSetId }
-      .ifEmpty { throw ValidationException("Invalid dataSetId in report: $dataSetId") }
-      .first()
-
-    return SingleReportProductDefinition(
-      id = definitionId,
-      name = productDefinition.name,
-      description = productDefinition.description,
-      metadata = productDefinition.metadata,
-      datasource = productDefinition.datasource.first(),
-      dataset = dataSet,
-      report = reportDefinition,
-    )
-  }
 
   fun save(definition: ProductDefinition) {
     definitions[definition.id] = definition
