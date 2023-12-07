544f57adbc51d7647829a2c92a086e18a9233ac6
Stu Pegg <stuart.pegg@digital.justice.gov.uk>
2023-12-07 09:28:35 +0000

    [DPR2-198] Update library dependency

--- src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportingtoolsapi/config/GsonConfig.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportingtoolsapi/config/GsonConfig.kt
@@ -5,17 +5,17 @@ import com.google.gson.GsonBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import uk.gov.justice.digital.hmpps.digitalprisonreportinglib.data.FilterTypeDeserializer
-import uk.gov.justice.digital.hmpps.digitalprisonreportinglib.data.IsoLocalDateTypeAdaptor
+import uk.gov.justice.digital.hmpps.digitalprisonreportinglib.data.IsoLocalDateTimeTypeAdaptor
 import uk.gov.justice.digital.hmpps.digitalprisonreportinglib.data.SchemaFieldTypeDeserializer
 import uk.gov.justice.digital.hmpps.digitalprisonreportinglib.data.model.FilterType
 import uk.gov.justice.digital.hmpps.digitalprisonreportinglib.data.model.ParameterType
-import java.time.LocalDate
+import java.time.LocalDateTime
 
 @Configuration
 class GsonConfig {
   @Bean
   fun deserialiser(): Gson = GsonBuilder()
-    .registerTypeAdapter(LocalDate::class.java, IsoLocalDateTypeAdaptor())
+    .registerTypeAdapter(LocalDateTime::class.java, IsoLocalDateTimeTypeAdaptor())
     .registerTypeAdapter(FilterType::class.java, FilterTypeDeserializer())
     .registerTypeAdapter(ParameterType::class.java, SchemaFieldTypeDeserializer())
     .create()
