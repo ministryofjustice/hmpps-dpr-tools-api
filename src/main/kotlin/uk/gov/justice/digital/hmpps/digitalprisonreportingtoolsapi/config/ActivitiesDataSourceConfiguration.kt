9e5469e0e633a005e51c2b5944b9248a203cb519
Gavriil <135604838+gavriil-g-moj@users.noreply.github.com>
2024-02-19 11:03:38 +0000

    DPR2-383 added activities data source (#25)
    * DPR2-383 added activities data source
    
    * DPR2-383 test on dev
    
    * DPR2-383 added DPD datasource name
    
    * DPR2-383 aattempt to use two named data sources
    
    * DPR2-383 disabling integration tests in attempt to check if the redshift query runs in Dev
    
    * DPR2-383 fix redshift driver key in application.yml
    
    * DPR2-383 attempt to fix driver error
    
    * DPR2-383 attempt2 to fix driver error
    
    * DPR2-383 enabling tests
    
    * DPR2-383 disabling tests
    
    * [DPR2-383] Fix JPA context
    
    * [DPR2-383] Linting
    
    * DPR2-383 revert circleci changes
    
    ---------
    
    Co-authored-by: Stu Pegg <stuart.pegg@digital.justice.gov.uk>

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportingtoolsapi/config/ActivitiesDataSourceConfiguration.kt
@@ -0,0 +1,26 @@
+package uk.gov.justice.digital.hmpps.digitalprisonreportingtoolsapi.config
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.jdbc.DataSourceBuilder
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import java.util.*
+import javax.sql.DataSource
+
+@Configuration
+class ActivitiesDataSourceConfiguration(
+  @Value("\${customdatasource.activities.url}") val url: String,
+  @Value("\${customdatasource.activities.username}") val username: String,
+  @Value("\${customdatasource.activities.password}") val password: String,
+  @Value("\${customdatasource.activities.driver}") val driver: String,
+) {
+  @Bean("activities")
+  fun createCustomDataSource(): DataSource {
+    return DataSourceBuilder.create()
+      .url(url)
+      .username(username)
+      .password(password)
+      .driverClassName(driver)
+      .build()
+  }
+}
