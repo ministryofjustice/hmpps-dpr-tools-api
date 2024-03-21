c60b92101485a4677432ff3f8072c7794b389c4a
Gavriil <135604838+gavriil-g-moj@users.noreply.github.com>
2024-03-21 14:30:51 +0000

    DPR2-503 bump lib version and remove component scan (#34)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportingtoolsapi/DigitalPrisonReportingToolsApi.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportingtoolsapi/DigitalPrisonReportingToolsApi.kt
@@ -1,13 +1,9 @@
 package uk.gov.justice.digital.hmpps.digitalprisonreportingtoolsapi
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
-@ConfigurationPropertiesScan
-@ComponentScan("uk.gov.justice.digital.hmpps.digitalprisonreportingtoolsapi", "uk.gov.justice.digital.hmpps.digitalprisonreportinglib")
 class DigitalPrisonReportingToolsApi
 
 fun main(args: Array<String>) {
