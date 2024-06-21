e94215b152210bb07bd4fc62e3db6caf61222c79
Stu Pegg <110236995+stalwartsolutions@users.noreply.github.com>
2024-06-21 16:50:39 +0100

    [DPR2-171] Bump library version (#39)
    * [DPR2-171] Bump library version
    
    * [DPR2-171] Bump library version

--- src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportingtoolsapi/service/FakeConfiguredApiService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportingtoolsapi/service/FakeConfiguredApiService.kt
@@ -1,14 +1,18 @@
 package uk.gov.justice.digital.hmpps.digitalprisonreportingtoolsapi.service
 
+import uk.gov.justice.digital.hmpps.digitalprisonreportinglib.data.AthenaApiRepository
 import uk.gov.justice.digital.hmpps.digitalprisonreportinglib.data.ConfiguredApiRepository
 import uk.gov.justice.digital.hmpps.digitalprisonreportinglib.data.ProductDefinitionRepository
+import uk.gov.justice.digital.hmpps.digitalprisonreportinglib.data.RedshiftDataApiRepository
 import uk.gov.justice.digital.hmpps.digitalprisonreportinglib.security.DprAuthAwareAuthenticationToken
 import uk.gov.justice.digital.hmpps.digitalprisonreportinglib.service.ConfiguredApiService
 
 class FakeConfiguredApiService(
   override val productDefinitionRepository: ProductDefinitionRepository,
   override val configuredApiRepository: ConfiguredApiRepository,
-) : ConfiguredApiService(productDefinitionRepository, configuredApiRepository) {
+  override val athenaApiRepository: AthenaApiRepository,
+  override val redshiftDataApiRepository: RedshiftDataApiRepository,
+) : ConfiguredApiService(productDefinitionRepository, configuredApiRepository, redshiftDataApiRepository, athenaApiRepository) {
 
   override fun validateAndFetchData(
     reportId: String,
