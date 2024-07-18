96b7587dea5a8a5cd952a5577faddafcf070a94b
Stu Pegg <110236995+stalwartsolutions@users.noreply.github.com>
2024-07-18 10:20:42 +0100

    [DPR2-778] Update library version for summary/aggregate templates (#53)
    * [DPR2-778] Update library version for summary/aggregate templates
    
    * [DPR2-778] Update library version for summary/aggregate templates
    
    * [DPR2-778] Update library version for summary/aggregate templates
    
    * [DPR2-778] Linting

--- src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportingtoolsapi/service/FakeConfiguredApiService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportingtoolsapi/service/FakeConfiguredApiService.kt
@@ -2,18 +2,22 @@ package uk.gov.justice.digital.hmpps.digitalprisonreportingtoolsapi.service
 
 import uk.gov.justice.digital.hmpps.digitalprisonreportinglib.data.AthenaApiRepository
 import uk.gov.justice.digital.hmpps.digitalprisonreportinglib.data.ConfiguredApiRepository
+import uk.gov.justice.digital.hmpps.digitalprisonreportinglib.data.DatasetHelper
 import uk.gov.justice.digital.hmpps.digitalprisonreportinglib.data.ProductDefinitionRepository
 import uk.gov.justice.digital.hmpps.digitalprisonreportinglib.data.RedshiftDataApiRepository
 import uk.gov.justice.digital.hmpps.digitalprisonreportinglib.data.model.Dataset
 import uk.gov.justice.digital.hmpps.digitalprisonreportinglib.security.DprAuthAwareAuthenticationToken
 import uk.gov.justice.digital.hmpps.digitalprisonreportinglib.service.ConfiguredApiService
+import uk.gov.justice.digital.hmpps.digitalprisonreportinglib.service.TableIdGenerator
 
 class FakeConfiguredApiService(
   override val productDefinitionRepository: ProductDefinitionRepository,
   override val configuredApiRepository: ConfiguredApiRepository,
   override val athenaApiRepository: AthenaApiRepository,
   override val redshiftDataApiRepository: RedshiftDataApiRepository,
-) : ConfiguredApiService(productDefinitionRepository, configuredApiRepository, redshiftDataApiRepository, athenaApiRepository) {
+  override val tableIdGenerator: TableIdGenerator,
+  override val datasetHelper: DatasetHelper,
+) : ConfiguredApiService(productDefinitionRepository, configuredApiRepository, redshiftDataApiRepository, athenaApiRepository, tableIdGenerator, datasetHelper) {
 
   override fun validateAndFetchData(
     reportId: String,
