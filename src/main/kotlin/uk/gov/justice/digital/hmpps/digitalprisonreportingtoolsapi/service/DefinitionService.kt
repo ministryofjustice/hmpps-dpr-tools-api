e42962adfa626b04364f17435f6e841d09fc1ce0
Stu Pegg <110236995+stalwartsolutions@users.noreply.github.com>
2024-07-18 10:20:42 +0100

    [DPR2-778] Update library version for summary/aggregate templates (#53)
    * [DPR2-778] Update library version for summary/aggregate templates
    
    * [DPR2-778] Update library version for summary/aggregate templates
    
    * [DPR2-778] Update library version for summary/aggregate templates
    
    * [DPR2-778] Linting

--- src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportingtoolsapi/service/DefinitionService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportingtoolsapi/service/DefinitionService.kt
@@ -1,13 +1,14 @@
 package uk.gov.justice.digital.hmpps.digitalprisonreportingtoolsapi.service
 
-import jakarta.validation.ValidationException
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.digitalprisonreportinglib.data.AthenaApiRepository
 import uk.gov.justice.digital.hmpps.digitalprisonreportinglib.data.ConfiguredApiRepository
+import uk.gov.justice.digital.hmpps.digitalprisonreportinglib.data.DatasetHelper
 import uk.gov.justice.digital.hmpps.digitalprisonreportinglib.data.RedshiftDataApiRepository
 import uk.gov.justice.digital.hmpps.digitalprisonreportinglib.data.model.ProductDefinition
 import uk.gov.justice.digital.hmpps.digitalprisonreportinglib.security.DprAuthAwareAuthenticationToken
 import uk.gov.justice.digital.hmpps.digitalprisonreportinglib.service.ReportDefinitionMapper
+import uk.gov.justice.digital.hmpps.digitalprisonreportinglib.service.TableIdGenerator
 import uk.gov.justice.digital.hmpps.digitalprisonreportingtoolsapi.data.InMemoryProductDefinitionRepository
 import uk.gov.justice.digital.hmpps.digitalprisonreportingtoolsapi.exception.InvalidDefinitionException
 
@@ -17,8 +18,10 @@ class DefinitionService(
   private val dataRepository: ConfiguredApiRepository,
   athenaApiRepository: AthenaApiRepository,
   redshiftDataApiRepository: RedshiftDataApiRepository,
+  tableIdGenerator: TableIdGenerator,
+  datasetHelper: DatasetHelper,
 ) {
-  val mapper: ReportDefinitionMapper = ReportDefinitionMapper(FakeConfiguredApiService(repository, dataRepository, athenaApiRepository, redshiftDataApiRepository))
+  val mapper: ReportDefinitionMapper = ReportDefinitionMapper(FakeConfiguredApiService(repository, dataRepository, athenaApiRepository, redshiftDataApiRepository, tableIdGenerator, datasetHelper), datasetHelper)
 
   fun saveAndValidate(
     definition: ProductDefinition,
@@ -26,9 +29,9 @@ class DefinitionService(
     originalBody: String,
   ) {
     try {
-      definition.id?.let {
+      definition.id.let {
         repository.save(definition, originalBody)
-      } ?: throw ValidationException("Definition missing id.")
+      }
       definition.report
         .map { report -> repository.getSingleReportProductDefinition(definitionId = definition.id, report.id) }
         // Attempt mapping to assert references are correct
@@ -45,10 +48,13 @@ class DefinitionService(
         )
       }
     } catch (e: Exception) {
-      definition.id?.let {
-        repository.deleteById(definition.id)
+      try {
+        definition.id.let {
+          repository.deleteById(definition.id)
+        }
+      } finally {
+        throw InvalidDefinitionException(e)
       }
-      throw InvalidDefinitionException(e)
     }
   }
 
