1f6fd8322cad704595f4ac97226b4be0137189cb
Gavriil <135604838+gavriil-g-moj@users.noreply.github.com>
2024-12-09 17:46:38 +0000

    DPR2-1075 upgrade lib (#280)
    * DPR2-1075 upgrade lib
    
    * DPR2-1075 upgrade lib

--- src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportingtoolsapi/data/InMemoryProductDefinitionRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportingtoolsapi/data/InMemoryProductDefinitionRepository.kt
@@ -13,14 +13,14 @@ class InMemoryProductDefinitionRepository : AbstractProductDefinitionRepository(
 
   private val definitions: ConcurrentHashMap<String, Pair<ProductDefinition, String>> = ConcurrentHashMap()
 
-  override fun getProductDefinitions(path: String?): List<ProductDefinition> {
+  override suspend fun getProductDefinitions(path: String?): List<ProductDefinition> {
     return definitions.values.map { it.first }.toList()
   }
 
-  override fun getProductDefinition(definitionId: String, dataProductDefinitionsPath: String?): ProductDefinition =
+  override suspend fun getProductDefinition(definitionId: String, dataProductDefinitionsPath: String?): ProductDefinition =
     definitions.getOrElse(definitionId) { throw DefinitionNotFoundException("Invalid report id provided: $definitionId") }.first
 
-  override fun getSingleReportProductDefinition(definitionId: String, reportId: String, dataProductDefinitionsPath: String?): SingleReportProductDefinition {
+  override suspend fun getSingleReportProductDefinition(definitionId: String, reportId: String, dataProductDefinitionsPath: String?): SingleReportProductDefinition {
     try {
       return super.getSingleReportProductDefinition(definitionId, reportId, null)
     } catch (e: ValidationException) {
