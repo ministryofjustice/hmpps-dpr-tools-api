cf73b5953a206d9f0b154b8efca0ab51b1674311
gavriil.gavriilidis <gavriil.gavriilidis@digital.justice.gov.uk>
2025-04-08 18:24:54 +0100

    DPR2-1873 test read super data type from redshift

--- src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportingtoolsapi/controller/DefinitionController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportingtoolsapi/controller/DefinitionController.kt
@@ -4,12 +4,10 @@ import com.google.gson.Gson
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import io.swagger.v3.oas.annotations.tags.Tag
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RestController
+import org.slf4j.LoggerFactory
+import org.springframework.web.bind.annotation.*
+import software.amazon.awssdk.services.redshiftdata.RedshiftDataClient
+import software.amazon.awssdk.services.redshiftdata.model.*
 import uk.gov.justice.digital.hmpps.digitalprisonreportinglib.data.model.ProductDefinition
 import uk.gov.justice.digital.hmpps.digitalprisonreportinglib.security.DprAuthAwareAuthenticationToken
 import uk.gov.justice.digital.hmpps.digitalprisonreportingtoolsapi.service.DefinitionService
@@ -19,7 +17,13 @@ import uk.gov.justice.digital.hmpps.digitalprisonreportingtoolsapi.service.Defin
 class DefinitionController(
   val definitionService: DefinitionService,
   val dprDefinitionGson: Gson,
+  val redshiftDataClient: RedshiftDataClient,
 ) {
+
+  companion object {
+    private val log = LoggerFactory.getLogger(this::class.java)
+  }
+
   @Operation(
     description = "Saves a definition",
     security = [SecurityRequirement(name = "bearer-jwt")],
@@ -51,4 +55,59 @@ class DefinitionController(
   )
   @GetMapping("/definitions/original/{definitionId}")
   fun getOriginalDefinition(@PathVariable definitionId: String) = definitionService.getOriginalBody(definitionId)
+
+  @Operation(
+    description = "Testing redshift retrieval",
+    security = [ SecurityRequirement(name = "bearer-jwt") ],
+  )
+  @GetMapping("/redshift/test/{column}")
+  fun testRedshift(@PathVariable column: String, @RequestParam executionId: String?): String {
+    val whereClause = executionId?.let { "WHERE current_execution_id = $executionId " } ?: ""
+    val result  = queryRedshiftAndGetResult("SELECT * from admin.execution_manager $whereClause;")
+    return getData("column", 0, result)
+  }
+
+  private fun queryRedshift(query:String): String {
+    val statementRequest = ExecuteStatementRequest.builder()
+      .clusterIdentifier("dpr-redshift-development")
+      .database("datamart")
+      .secretArn("arn:aws:secretsmanager:eu-west-2:771283872747:secret:dpr-redshift-secret-development-rLHcQZ")
+      .sql(query)
+      .build()
+
+    val executionId = redshiftDataClient.executeStatement(statementRequest).id()
+    log.debug("Executed admin table statement and got ID: $executionId")
+    val describeStatementRequest = DescribeStatementRequest.builder()
+      .id(executionId)
+      .build()
+    var describeStatementResponse: DescribeStatementResponse
+    do {
+      Thread.sleep(500)
+      describeStatementResponse = redshiftDataClient.describeStatement(describeStatementRequest)
+      if (describeStatementResponse.status() == StatusString.FAILED) {
+        throw RuntimeException("Statement with execution ID: $executionId failed.")
+      } else if (describeStatementResponse.status() == StatusString.ABORTED) {
+        throw RuntimeException("Statement with execution ID: $executionId was aborted.")
+      }
+    }
+    while (describeStatementResponse.status() != StatusString.FINISHED)
+    return executionId
+  }
+
+  private fun getRedshiftStatementResult(executionId: String): GetStatementResultResponse {
+    val getStatementResultRequest = GetStatementResultRequest.builder()
+      .id(executionId)
+      .build()
+    return redshiftDataClient.getStatementResult(getStatementResultRequest)
+  }
+
+  private fun queryRedshiftAndGetResult(query: String): GetStatementResultResponse {
+    return getRedshiftStatementResult(queryRedshift(query))
+  }
+
+  private fun getData(columnName: String, rowNumber: Int, getStatementResultResponse: GetStatementResultResponse): String {
+    val columnNameToResultIndex = mutableMapOf<String, Int>()
+    getStatementResultResponse.columnMetadata().forEachIndexed{ i, colMetaData -> columnNameToResultIndex[colMetaData.name()] = i}
+    return getStatementResultResponse.records()[rowNumber][columnNameToResultIndex[columnName]!!].stringValue()
+  }
 }
