bcf82769f64ecd9151508fe3912081c32c366899
madhuri.mahesh <madhuri.mahesh@justice.gov.uk>
2026-04-29 12:04:32 +0100

    DPR2-2696 - added DateUtilSerializer and DateUtilDeserializer

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportingtoolsapi/data/DateUtilSerializer.kt
@@ -0,0 +1,19 @@
+package uk.gov.justice.digital.hmpps.digitalprisonreportinglib.data
+
+import org.springframework.boot.jackson.JacksonComponent
+import tools.jackson.core.JsonGenerator
+import tools.jackson.databind.SerializationContext
+import tools.jackson.databind.ser.std.StdSerializer
+import java.text.SimpleDateFormat
+import java.util.Date
+
+@JacksonComponent
+class DateUtilSerializer : StdSerializer<Date>(Date::class.java) {
+
+  private val formatter = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+
+  override fun serialize(value: Date, gen: JsonGenerator, ctxt: SerializationContext) {
+    val formattedDate = formatter.format(value)
+    gen.writeString(formattedDate)
+  }
+}
