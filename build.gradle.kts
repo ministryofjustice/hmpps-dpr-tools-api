402cc886a1795a03423114183d244b9096942678
djcostin <daniel.costin@gmail.com>
2026-07-10 12:59:18 +0100

    DPR2-2958 update gradle plugin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "10.0.5"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "11.0.0-beta"
   kotlin("jvm") version "2.4.0"
   kotlin("plugin.spring") version "2.4.0"
   id("jacoco")
