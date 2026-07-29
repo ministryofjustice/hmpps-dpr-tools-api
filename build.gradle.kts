ee96ae24f8a1fb377a5f871b5ff5c71307291b93
djcostin <daniel.costin@gmail.com>
2026-07-29 12:54:56 +0100

    DPR2-3062 upgrade spring boot plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "11.0.0-beta2"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "11.0.0"
   kotlin("jvm") version "2.4.0"
   kotlin("plugin.spring") version "2.4.0"
   id("jacoco")
