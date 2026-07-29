cb4a4c742425d1eb56f86da9cc95e6c997179a11
djcostin <daniel.costin@digital.justice.gov.uk>
2026-07-29 13:18:35 +0100

    Dpr2 3062 sec fixes (#827)
    * DPR2-3062 upgrade spring boot plugin
    
    * DPR2-3062 upgrade spring boot plugin and spring boot starter
    
    ---------
    
    Co-authored-by: djcostin <daniel.costin@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "11.0.0"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "11.0.2"
   kotlin("jvm") version "2.4.0"
   kotlin("plugin.spring") version "2.4.0"
   id("jacoco")
@@ -20,7 +20,7 @@ dependencies {
   implementation("software.amazon.awssdk:redshiftdata:2.31.24")
   implementation("software.amazon.awssdk:athena:2.31.65")
   implementation("software.amazon.awssdk:sts:2.31.65")
-  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:3.0.0-beta")
+  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:3.0.0")
 
   // Security
   implementation("org.springframework.boot:spring-boot-starter-security")
